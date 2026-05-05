7e934c29d659fafd413cf3fc9cd50edb8cde47ef
sumit haverikar <sumeet.haverikar@mindbowser.com>
2026-05-05 18:44:40 +0530

    feat-created login and forgot password page

---
+++ src/components/ui/navbar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { HelpCircle } from "lucide-react";
+
+export default function Navbar() {
+  return (
+    <header className="w-full h-[65px] border-b bg-white flex items-center justify-between px-6">
+      <div className="font-inter text-[#1E40AF] font-semibold text-lg">
+        BedWatch
+      </div>
+
+      <nav className="hidden md:flex items-center gap-6 text-sm text-gray-600">
+        <span className="cursor-pointer hover:text-black"> Ward View</span>
+        <span className="cursor-pointer hover:text-black"> staffing</span>
+        <span className="cursor-pointer hover:text-black"> Resource Map</span>
+        <span className="cursor-pointer hover:text-black"> Reports </span>
+          </nav>
+          
+          <div className="flex items-center gap-4">
+              <HelpCircle className="w-5 h-5 text-gray-500 cursor-pointer" />
+              
+              <Avatar className="w-8 h-8">
+                  <AvatarImage src="" />
+                  <AvatarFallback>U</AvatarFallback>
+                  
+              </Avatar>
+              
+          </div>
+    </header>
+  );
+}
---
+++ src/pages/ForgotPassword.tsx
@@ -0,0 +1,218 @@
+import React from "react";
+import Navbar from "@/components/ui/navbar";
+import { IoShieldCheckmarkOutline } from "react-icons/io5";
+import { TbShieldSearch } from "react-icons/tb";
+import { Card } from "@/components/ui/card";
+import { Eye, EyeOff } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import zxcvbn from "zxcvbn";
+import { IoIosArrowRoundForward } from "react-icons/io";
+
+function ForgotPassword() {
+  const [show, setShow] = useState(false);
+  const [password, setPassword] = useState("");
+  const [score, setScore] = useState(0);
+  const [showConfirm, setShowConfirm] = useState(false);
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setPassword(value);
+
+    const result = zxcvbn(value);
+    setScore(result.score);
+  };
+
+  const strengthText = ["WEAK", "FAIR", "GOOD", "STRONG", "VERY STRONG"];
+
+  return (
+    <div className="flex flex-col min-h-screen bg-[#FBF8FF]">
+      <Navbar />
+
+      <div className="flex flex-1">
+        <div className="hidden md:flex flex-1 items-center justify-center px-12">
+          <div className="max-w-xl w-full">
+            <p className=" font-inter text-[11px] font-semibold tracking-widest text-blue-700">
+              SECURITY PROTOCOL A4
+            </p>
+
+            <h1 className="font-inter text-[24px] font-bold mt-3 text-gray-900">
+              Secure Your Clinical Access
+            </h1>
+
+            <p className="font-inter text-sm text-gray-500 mt-3 leading-6">
+              To maintain patient data integrity and hospital security
+              standards, all first-time logins require a mandatory password
+              update.
+            </p>
+
+            <div className="mt-6 p-4 bg-[#F4F2FC] border rounded-md space-y-4 max-w-md">
+              <div className="flex gap-3">
+                <IoShieldCheckmarkOutline className="size-[20px]" />
+                <div>
+                  <p className="font-inter text-sm font-medium">
+                    Enterprise Encryption
+                  </p>
+                  <p className="font-inter text-xs text-gray-500">
+                    SHA-256 protocols within secure network
+                  </p>
+                </div>
+              </div>
+
+              <div className="flex gap-3">
+                <TbShieldSearch className="size-[20px]" />
+                <div>
+                  <p className="font-inter text-sm font-medium">
+                    Compliance Standards
+                  </p>
+                  <p className="font-inter text-xs text-gray-500">
+                    HIPAA & GDPR compliance
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-1 items-center justify-center px-4">
+          <Card className="w-full max-w-md border border-gray-300 rounded-sm shadow-sm">
+            <div className="p-6 space-y-5">
+              <h2 className="font-inter text-lg font-semibold">
+                Force Password Reset
+              </h2>
+
+              <p className="font-inter text-[#444653] text-[14px]">
+                Choose a password that you haven't used before for this account.
+              </p>
+
+              <div className="w-full">
+                <p className="font-inter text-[#757684] text-[12px] mb-1">
+                  NEW PASSWORD
+                </p>
+
+                <div className="relative">
+                  <Input
+                    type={show ? "text" : "password"}
+                    className="h-[42px] pr-10"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    placeholder="••••••••"
+                  />
+
+                  <div className="absolute inset-y-0 right-3 flex items-center">
+                    {show ? (
+                      <EyeOff
+                        onClick={() => setShow(false)}
+                        className="h-4 w-4 text-gray-400 cursor-pointer "
+                      />
+                    ) : (
+                      <Eye
+                        onClick={() => setShow(true)}
+                        className="h-4 w-4 text-gray-400 cursor-pointer"
+                      />
+                    )}
+                  </div>
+                </div>
+
+                <div className="mt-2">
+                  <div className="flex justify-between text-[10px] text-gray-500">
+                    <span>PASSWORD STRENGTH</span>
+                    <span className="text-[#006780] font-medium">
+                      {strengthText[score]}
+                    </span>
+                  </div>
+
+                  <div className="flex gap-1 mt-1">
+                    {[0, 1, 2, 3].map((i) => (
+                      <div
+                        key={i}
+                        className={`h-1 flex-1 rounded ${
+                          i <= score ? "bg-[#006780]" : "bg-gray-200"
+                        }`}
+                      />
+                    ))}
+                  </div>
+                </div>
+
+                <div className="grid grid-cols-2 gap-2 text-xs mt-3">
+                  <p
+                    className={
+                      password.length >= 12 ? "text-[#006780]" : "text-gray-400"
+                    }
+                  >
+                    ✔ At least 12 characters
+                  </p>
+
+                  <p
+                    className={
+                      /[@$!%*?&#]/.test(password)
+                        ? "text-[#006780]"
+                        : "text-gray-400"
+                    }
+                  >
+                    ✔ One special character
+                  </p>
+
+                  <p
+                    className={
+                      /[A-Z]/.test(password)
+                        ? "text-[#006780]"
+                        : "text-gray-400"
+                    }
+                  >
+                    ✔ One uppercase letter
+                  </p>
+
+                  <p
+                    className={
+                      /[0-9]/.test(password)
+                        ? "text-[#006780]"
+                        : "text-gray-400"
+                    }
+                  >
+                    ✔ One numerical digit
+                  </p>
+                </div>
+
+                <div className="mt-4">
+                  <p className="text-[#757684] text-[12px] mb-1">
+                    CONFIRM NEW PASSWORD
+                  </p>
+
+                  <div className="relative">
+                    <Input
+                      type={showConfirm ? "text" : "password"}
+                      className="h-[42px] pr-10"
+                      placeholder="••••••••"
+                    />
+                    <div className="absolute inset-y-0 right-3 flex items-center">
+                      {showConfirm ? (
+                        <EyeOff
+                          onClick={() => setShowConfirm(false)}
+                          className="h-4 w-4 text-gray-400 cursor-pointer"
+                        />
+                      ) : (
+                        <Eye
+                          onClick={() => setShowConfirm(true)}
+                          className="h-4 w-4 text-gray-400 cursor-pointer"
+                        />
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-center items-center bg-[#00288E] h-[56px]">
+                <button className="w-full h-[56px] bg-[#00288E] hover:bg-[#001f6b] text-white flex items-center justify-center gap-2 rounded-sm">
+                  Set Password & Continue
+                  <IoIosArrowRoundForward className="size-5" />
+                </button>
+              </div>
+            </div>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ForgotPassword;
