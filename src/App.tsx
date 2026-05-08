059e6d9f7e96c4d2c88ea4f929f5c8a8931c3a16
Amreetanshu Sinha <amreetanshu.sinha@mindbowser.com>
2026-05-08 14:50:20 +0530

    feat- added srStaff dashboard and transfer service

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Login from "./pages/Login";
 import Dashboard from "./components/Admin/Dashboard";
 import ForgotPassword from "./pages/ForgotPassword";
 import Layout from "./components/layout/Layout";
+import SrStaffDashboard from "./components/SrStaff/DashBoard";
+import TransfersList from "./components/SrStaff/InterWardTransfer";
 function App() {
   return (
     <BrowserRouter>
@@ -12,6 +14,9 @@ function App() {
         <Route element={<Layout />}>
           <Route path="/admin-dashboard" element={<Dashboard />} />
         </Route>
+
+        <Route path="/sr-staff/dashboard" element={<SrStaffDashboard />} />
+        <Route path="/sr-staff/transfers" element={<TransfersList />} />
 
         <Route path="ForgotPassword" element={<ForgotPassword />} />
       </Routes>
