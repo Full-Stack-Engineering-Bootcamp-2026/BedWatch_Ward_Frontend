c98e907018c87e56060f02f0fb64876de07f4ae8
sumit haverikar <sumeet.haverikar@mindbowser.com>
2026-05-11 12:55:37 +0530

    fix:some minor changes in ui

--- src/components/Staff/Pages/NewAdmission.tsx
+++ src/components/Staff/Pages/NewAdmission.tsx
@@ -163,7 +163,7 @@ export default function NewAdmissionPage() {
 
           <button
             onClick={handleSubmit}
-            className="bg-blue-700 text-white px-4 py-2 rounded-md text-sm"
+            className="bg-[#1E40AF] text-white px-4 py-2 rounded-md text-sm"
           >
             Complete Admission
           </button>
