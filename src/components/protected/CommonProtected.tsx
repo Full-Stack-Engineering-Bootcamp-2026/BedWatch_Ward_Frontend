8d35d9ac1b5840adf3a2ad318d34ecb3e6aa23db
indraphand-mb <indra.phand@mindbowser.com>
2026-05-12 14:31:01 +0530

    fix : common layout fixed.

--- src/components/protected/CommonProtected.tsx
+++ src/components/protected/CommonProtected.tsx
@@ -1,15 +1,17 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 import { useSelector } from "react-redux";
-
-export default function AdminProtectedRoute() {
+type commonProps = {
+  Role: String;
+};
+export default function CommonProtectedRoute({ Role }: commonProps) {
   const { user, token } = useSelector((state: any) => state.auth);
 
   if (!token) {
     return <Navigate to="/" replace />;
   }
 
-  if (user?.role !== "ADMIN") {
+  if (user?.role !== Role) {
     return <Navigate to="/" replace />;
   }
 
